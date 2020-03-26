88e3e057727861ecd34161b016cc5cc7dd33d3c7
cse-akash <aahsansadik@gmail.com>
2020-03-27 00:52:56 +0600

    Primary stage completed

---
+++ LMS/LMS/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LMS.Models;
+
+namespace LMS.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Random()
+        {
+            var book = new Book() { BookName = "Harry Potter" };
+            return View(book);
+        }
+    }
+}
---
+++ LMS/LMS/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string BookAuthor { get; set; }
+    }
+}
